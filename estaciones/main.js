bbc0cb9f8297f43714bbdb6e006f937057456b07
franromeroe <franromeroeze@gmail.com>
2022-12-31 11:00:20 -0300

    estaciones

--- estaciones/main.js
+++ estaciones/main.js
@@ -12,13 +12,16 @@ function mostrar()
 		case "Abril":
 		case "Mayo" :
 			// alert("Arrancamos con el frio");
-            elemento.style.backgroundColor = "red";
+            // elemento.style.backgroundColor = "red";
+            elemento.style.backgroundImage = "url(https://www.dzoom.org.es/wp-content/uploads/2018/10/foto-otono-1.jpg)";
+            elemento.style.backgroundPosition = "center center";
 			break;
 
 		case "Junio":
 		case "Julio":
 		case "Agosto":
-			alert("Hace mucho frio!!");
+			// alert("Hace mucho frio!!");
+            elemento.style.backgroundImage = "url(https://media.istockphoto.com/id/1140829787/es/foto/sunset-en-savannah-plains.jpg?b=1&s=612x612&w=0&k=20&c=Xii4-SeUmyDUcJbnCeB1NycueCtnSkzqDd1AUB1Zxoo=)"
 			break;
 
 		case "Septiembre" :
