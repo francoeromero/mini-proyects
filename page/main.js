e04bd1afb8a42f9639eea9aa4b7007e084472a43
franromeroe <franromeroeze@gmail.com>
2023-01-08 17:10:02 -0300

    pagina principal estructura de diseño

---
+++ page/main.js
@@ -0,0 +1,12 @@
+const grid = new Muuri('.grid', {
+    layout: {
+    rounding: false
+    }
+});
+    
+    
+window.addEventListener('load', () => {
+ grid.refreshItems().layout();
+document.getElementById('grid').classList.add('imagenes-cargadas');
+ });
+    
