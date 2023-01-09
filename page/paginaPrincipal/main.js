53a723f9a74fc9c8827e78c26c93311d2ff3d53d
franromeroe <franromeroeze@gmail.com>
2023-01-09 01:55:57 -0300

    responsive

--- page/paginaPrincipal/main.js
+++ page/paginaPrincipal/main.js
@@ -38,3 +38,57 @@ window.addEventListener('load', () => {
 
 	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY >0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navbar.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+	distance: '25px',
+	duration: 2500,
+	reset: true
+})
+
+sr.reveal('.home-text',{delay:190, origin:'bottom'})
+
+sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
+
+
+
+
+document.querySelectorAll(".gallery__img img").forEach(el=>{
+	el.addEventListener("click", function(ev){
+		this.parentNode.classList.add("active");
+	})
+});
+
+
