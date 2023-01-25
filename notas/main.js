28c220ffca591bc39d196fa3d8c198d103365539
franromeroe <franromeroeze@gmail.com>
2023-01-25 17:48:53 -0300

    diseño

--- notas/main.js
+++ notas/main.js
@@ -1,8 +1,23 @@
 const addButton = document.getElementById("add-button");
 const noteInput = document.getElementById("note");
 const notesContainer = document.getElementById("notes-container");
+const form = document.querySelector("form");
 
-addButton.addEventListener("click", () => {
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+// Agregar evento de click al botón "Agregar"
+addButton.addEventListener("click", addNote);
+
+// Agregar evento de keypress al input de notas
+noteInput.addEventListener("keypress", (event) => {
+  if (event.keyCode === 13) {
+    addNote();
+  }
+});
+
+function addNote() {
   const note = noteInput.value;
   if (note) {
     const noteElement = document.createElement("p");
@@ -10,4 +25,4 @@ addButton.addEventListener("click", () => {
     notesContainer.appendChild(noteElement);
     noteInput.value = "";
   }
-});
+}
