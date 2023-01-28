5431795b4235537c28453b0233a7de77a15eafa3
franromeroe <franromeroeze@gmail.com>
2023-01-27 22:01:59 -0300

    notes

--- notas/main.js
+++ notas/main.js
@@ -24,13 +24,24 @@ function addNote() {
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.classList.add("task-checkbox");
+    checkbox.style.height = "20px";
+    checkbox.style.scale = "1.8";
+    checkbox.style.margin = "15px";
+   
     noteElement.appendChild(checkbox);
     noteElement.appendChild(document.createTextNode(note));
     notesContainer.appendChild(noteElement);
+
+    noteElement.style.padding = "10px";
+    noteElement.style.display = "flex";
+    noteElement.style.flexDirection = "column";
+   
+    notesContainer.style.transition = "1s";
     noteInput.value = "";
 
     checkbox.addEventListener("change", (event) => {
       if (event.target.checked) {
+        
         noteElement.classList.add("completed");
       } else {
         noteElement.classList.remove("completed");
