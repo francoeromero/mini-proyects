8548422d14be619e04656b8b31d82affa02d8f0e
franromeroe <franromeroeze@gmail.com>
2023-01-28 18:24:35 -0300

    practice

--- galeriaVideos/main.js
+++ galeriaVideos/main.js
@@ -15,6 +15,17 @@ let youtubeContainer = document.querySelector(".youtube-video-container");
 let youtubeUrl = "https://www.youtube.com/embed/pYpKlBaREII?autoplay=1&mute=1&controls=1&modestbranding=1&rel=0";
 
 shortVideo.addEventListener("click", function(){
-    youtubeContainer.style.display = "block";
+    youtubeContainer.style.display = "flex";
     youtubeContainer.querySelector("iframe").src = youtubeUrl;
+});
+
+
+//la x
+
+const closeBtn = document.querySelector('.close-btn');
+const videoContainer = document.querySelector('.youtube-video-container');
+
+closeBtn.addEventListener('click', function() {
+  
+  videoContainer.style.display = 'none';
 });
