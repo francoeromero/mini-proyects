728612c85284c60c2517e56a55ce94a17d78461c
franromeroe <franromeroeze@gmail.com>
2023-02-01 18:15:45 -0300

    palindromo

---
+++ palindromo/main.js
@@ -0,0 +1,19 @@
+function palindromo() {
+    let cadena = document.getElementById("frase").value.toLowerCase();
+    //elimino los caracteres que no son alfanumericos
+    cadena = cadena.match(/[a-z0-9]/g);
+    //comparo la cadena original con su version invertida
+    let esPalindromo = cadena.join('') === cadena.reverse().join('');
+
+    if(esPalindromo === true){
+    esPalindromo = "es palindromo";
+    } else if (esPalindromo === false){
+        esPalindromo = "no es palindromo";
+    }
+    document.getElementById("resultado").innerHTML = esPalindromo;
+
+  }
+
+
+//   let resultado = palindromo("eye");
+//   document.getElementById("resultado").innerHTML = resultado;
